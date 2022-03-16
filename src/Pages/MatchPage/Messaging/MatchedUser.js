4e022884403101b3917f9dac43b3f5ceebfffc12
kjebali23 <khalil.jebali.kj@gmeil.com>
2022-03-16 23:31:46 +0100

    replaced placeholder Matchs data in the messaging section with data from the DB

--- src/Pages/MatchPage/Messaging/MatchedUser.js
+++ src/Pages/MatchPage/Messaging/MatchedUser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useInfo } from '../../../Hooks/useUserData';
 
 const MatchedUser = (props) => {
 
@@ -9,14 +10,22 @@ const MatchedUser = (props) => {
     //     props.changeDisplayMessage(!props.messageState)
     // }
 
+    const {error , loading , data} = useInfo(props.match);
+    if(error){return <div>An Error occured</div>}
+    if(loading){return <div>Loading</div>}
+
+
+    const matchInfo = data.getUser;
+    // console.log(matchInfo)
+
   return (
       <>
                 <li className='list-item' onClick={()=> changeMatch(props.userId)} >
-                    <img src={props.userImage} />
+                    <img src={matchInfo.Images[0]} />
                 
                 <div className='match-info'>
-                    <h3>{props.userName}</h3>
-                    <p>Matched 20 Days ago</p>
+                    <h3>{matchInfo.UserName}</h3>
+                    {/* <p>Matched 20 Days ago</p> */}
                 </div>
                 </li>
       </>
--- src/Pages/MatchPage/Messaging/Messaging.js
+++ src/Pages/MatchPage/Messaging/Messaging.js
@@ -3,44 +3,14 @@ import MatchedUser from './MatchedUser'
 import MessagingBox from './MessagingBox';
 import './Messaging.css';
 import BackBtn from '../Profile/Parameters/BackBtn';
-import {useInfo} from '../../../Hooks/useUserData'
-
 
 
 const Messaging = (props) => {
 
     const [usermatchState , setUsermatchState] = useState(null);
-    const users=[
-        {
-            id:'1',
-            name:'khalil',
-            Images:[ 'https://images.unsplash.com/photo-1631819825505-4291831187ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWF6ZGElMjBtaWF0YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60']  ,  
-            Messages:[`user 1's First Message` , 'Second Message', '3rd Message' ,'4th Message' ,'5th Message' ]
-        },
-            {
-            id:'2',
-            name:'Jebali',
-            Images:[ 'https://images.unsplash.com/photo-1607603750916-eaf866bc907d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsYWNrJTIwc3VwcmF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60']   
-            ,Messages:[`user 2's First Message` , 'Second Message', '3rd Message' ] }
-            ,
-            {
-                id:'3',
-                name:'Jebali',
-                Images:[ 'https://images.unsplash.com/photo-1607603750916-eaf866bc907d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsYWNrJTIwc3VwcmF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60']    
-                ,Messages:[`user 3's First Message` , 'Message' ]}
-            ,{
-                id:'4',
-                name:'Jebali',
-                Images:[ 'https://images.unsplash.com/photo-1607603750916-eaf866bc907d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsYWNrJTIwc3VwcmF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60']   
-                ,Messages:[`user 4's First Message` ,'Message' ] }
-            ]
+    
 
   let width = window.innerWidth;
-
-
-
-  console.log(props)
-  
   
   if(width > 550){
     return (
@@ -50,8 +20,8 @@ const Messaging = (props) => {
         <div className='user-matchs-section'>
             <BackBtn/>
             <ul className='matchs-list'>
-            {users.map((user)=>
-            <MatchedUser userName={user.name} userImage={user.Images} userId={user.id} messageBox={setUsermatchState}/>
+            {props.userMatchs.map((match)=>
+            <MatchedUser match={match}/>
             )}
             </ul>
         </div>
@@ -66,7 +36,9 @@ const Messaging = (props) => {
                 </div>
             </div>}
            {usermatchState !== null &&
-            <MessagingBox usersMessages={users[usermatchState].Messages} />
+            <MessagingBox 
+            // usersMessages={users[usermatchState].Messages} 
+            />
             }
 
         </div>
@@ -82,16 +54,18 @@ const Messaging = (props) => {
     <div className='user-matchs-section'>
         <BackBtn/> 
         <ul className='matchs-list'>
-        {users.map((user)=>
-        <MatchedUser userName={user.name} userImage={user.Images} userId={user.id} messageBox={setUsermatchState} 
-        />
-        )}
+
+        {props.userMatchs.map((match)=>
+            <MatchedUser match={match}/>
+            )}
         </ul>
     </div>}
     
     {usermatchState !== null &&
     <div className='message-box-section'>
-        <MessagingBox usersMessages={users[usermatchState].Messages}/>
+        <MessagingBox 
+        // usersMessages={users[usermatchState].Messages}
+        />
     </div>}
    
    </div>
